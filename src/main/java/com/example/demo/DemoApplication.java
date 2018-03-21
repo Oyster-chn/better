c973ee84897eef0e09bd6dff3f41abe945196b0a
Oyster-chn <1136890590@qq.com>
2018-03-21 10:18:22 +0800

    和

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("123");
+		System.out.println("1234");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 }
